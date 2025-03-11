1ede5e20e34e3baec526baf3e1e6bff9d609d3ab
Arcani-Dominus <arcanidominus@gmail.com>
2025-03-12 04:56:28 +0530

    Update levels.js

--- levels.js
+++ levels.js
@@ -5,8 +5,8 @@ import { getDoc, doc, updateDoc } from "https://www.gstatic.com/firebasejs/10.5.
 // 🔥 Define Riddles & Answers
 const riddles = {
     2: "The more you take, the more you leave behind. What am I?",
-    3: ".. / .... ..- -- / -... ..- - / .... .- ...- . / -. --- / -- --- ..- - .... --..--  \n .. / -... .-. . .- - .... . / -... ..- - / .... .- ...- . / -. --- / .-.. ..- -. --. ... --..-- \n .-- .... . -. / -.. .- .-. -.- -. . ... ... / ..-. .- .-.. .-.. ... --..-- / .. / -.- . . .--. / - .... . / .-.. .. --. .... - ... / .- .-.. .. ...- . .-.-.- \n .-- .... .- - / .- -- / .. ..--.. \n Find Answer Near Me!",
-    4: "Coordniates \n Find Me!",
+    3: "Coordniates \n Find Me!",
+    4: ".. / .... ..- -- / -... ..- - / .... .- ...- . / -. --- / -- --- ..- - .... --..--  \n .. / -... .-. . .- - .... . / -... ..- - / .... .- ...- . / -. --- / .-.. ..- -. --. ... --..-- \n .-- .... . -. / -.. .- .-. -.- -. . ... ... / ..-. .- .-.. .-.. ... --..-- / .. / -.- . . .--. / - .... . / .-.. .. --. .... - ... / .- .-.. .. ...- . .-.-.- \n .-- .... .- - / .- -- / .. ..--.. \n Find Answer Near Me!",
     5: "I lead you up, I lead you down, Yet in my shadow, secrets are found.Not the front, but the hidden way, Find me where few choose to stay."
 };
 const answers = {
